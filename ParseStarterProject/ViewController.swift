7802b97c64aee8d3170374a17def78fe254a9fae
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-03-14 16:37:37 +0100

    Add spinner

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -15,32 +15,48 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var passwordField: UITextField!
+    //Add activity indicators for spinners and alerts.
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    
     
     @IBAction func signUp(sender: UIButton) {
         
         // Error message if details are incorrect.
         if username.text == "" || passwordField.text == "" {
             //Create alert.
-            var alert = UIAlertController(title: "Oops!", message: "Please enter a valid username and password!", preferredStyle: UIAlertControllerStyle.Alert)
+            let alert = UIAlertController(title: "Oops!", message: "Please enter a valid username and password.", preferredStyle: UIAlertControllerStyle.Alert)
             alert.addAction((UIAlertAction(title: "Got it!", style: .Default, handler: { (action) -> Void in
                 //Get rid of the alert.
                 self.dismissViewControllerAnimated(true, completion: nil)
             })))
             //Display the alert
             self.presentViewController(alert, animated: true, completion: nil)
+        } else {
+            //Create spinner
+            activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0, 0, 50, 50))
+            //Position it.
+            activityIndicator.center = self.view.center
+            //Hide when stopped.
+            activityIndicator.hidesWhenStopped = true
+            activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+            view.addSubview(activityIndicator)
+            //activate activityIndicator
+            activityIndicator.startAnimating()
+            UIApplication.sharedApplication().beginIgnoringInteractionEvents()
         }
         
     }
     @IBAction func logIn(sender: UIButton) {
     }
-      //Add activity indicators for spinners and alerts.
-    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
 
+    }
+    
+    // Make keyboard disappear when user clicks outside of text field.
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
 
