4fa28e5cfbaad29fac83050bc545c1b3f4583ed3
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-03-14 16:22:45 +0100

    Add alert functionality

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -13,6 +13,26 @@ import Parse
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBAction func signUp(sender: UIButton) {
+        
+        // Error message if details are incorrect.
+        if username.text == "" || passwordField.text == "" {
+            //Create alert.
+            var alert = UIAlertController(title: "Oops!", message: "Please enter a valid username and password!", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction((UIAlertAction(title: "Got it!", style: .Default, handler: { (action) -> Void in
+                //Get rid of the alert.
+                self.dismissViewControllerAnimated(true, completion: nil)
+            })))
+            //Display the alert
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+        
+    }
+    @IBAction func logIn(sender: UIButton) {
+    }
       //Add activity indicators for spinners and alerts.
     var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
     
